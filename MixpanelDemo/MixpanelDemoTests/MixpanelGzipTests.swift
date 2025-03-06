49bea1896a985aa2df3b17b0f4394627bc285507
m0hitnnd <mohitanand95.ma@gmail.com>
2025-03-07 02:10:34 +0530

    test changes

--- MixpanelDemo/MixpanelDemoTests/MixpanelGzipTests.swift
+++ MixpanelDemo/MixpanelDemoTests/MixpanelGzipTests.swift
@@ -1,28 +1,55 @@
 import XCTest
+import zlib
 @testable import Mixpanel
 
-class MixpanelGzipTests: XCTestCase {
-
-    func testGzipCompression() {
-        // Test data compression
-        let testData = String(repeating: "Test data for compression ", count: 100).data(using: .utf8)!
-        let compressedData = testData.gzipped()
-
-        XCTAssertNotNil(compressedData, "Data compression should succeed")
-        XCTAssertLessThan(compressedData!.count, testData.count, "Compressed data should be smaller than original")
-
+class MixpanelGzipTests: MixpanelBaseTests {
+    
+    func testGzipNotSameAsOriginal() {
+        let originalString = String.randomString(length: 100)
+        guard let originalData = originalString.data(using: .utf8) else {
+            XCTFail("Failed to create data from string")
+            return
+        }
+        
+        guard let ourGzippedData = originalData.gzipped() else {
+            XCTFail( "Gzipped data from wrapper should not be nil")
+            return
+        }
+        XCTAssertNotEqual(
+            ourGzippedData,
+            originalData,
+            "Gzipped data should not be equal to original data"
+        )
+    }
+    
+    func testGzipEmptyData() {
+        let emptyData = Data()
+        let compressed = emptyData.gzipped()
+        XCTAssertNil(compressed, "Empty data should result in nil gzip output")
+    }
+    
+    func testGzipIntegrationForInstance() {
         // Test Mixpanel integration
         let testMixpanel = Mixpanel.initialize(token: randomId(), trackAutomaticEvents: true, flushInterval: 60)
         XCTAssertFalse(testMixpanel.useGzipCompression, "GZIP compression should be disabled by default")
-
+        
         testMixpanel.useGzipCompression = true
         XCTAssertTrue(testMixpanel.useGzipCompression, "GZIP compression should be enabled after setting")
-
+        
         // Verify the setting is passed through to FlushRequest
         XCTAssertTrue(testMixpanel.flushInstance.useGzipCompression, "GZIP compression setting should be passed to flush instance")
     }
+}
 
-    private func randomId() -> String {
-        return String(format: "%08x%08x", arc4random(), arc4random())
+private extension String {
+    
+    /// Generate random letters string for test.
+    static func randomString(length: Int) -> String {
+        
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+        let characters = (0..<length).map { _ in letters.randomElement()! }
+        
+        return String(characters)
     }
 }
+
