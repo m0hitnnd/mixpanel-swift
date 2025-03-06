9e7bc56724807e6384576621ef95c45c0856f881
m0hitnnd <mohitanand95.ma@gmail.com>
2025-03-07 01:17:00 +0530

    gzip handled added back

--- Sources/FlushRequest.swift
+++ Sources/FlushRequest.swift
@@ -33,19 +33,22 @@ class FlushRequest: Network {
             }
             return nil
         }
-
-        var resourceHeaders: [String: String] = ["Content-Type": "application/json"].merging(headers) {(_,new) in new }
+        
+        var resourceHeaders = ["Content-Type": "application/json"]
+        if useGzipCompression {
+            resourceHeaders["Content-Encoding"] = "gzip"
+        }
+        resourceHeaders = resourceHeaders.merging(headers) {(_,new) in new }
 
         let ipString = useIP ? "1" : "0"
         var resourceQueryItems: [URLQueryItem] = [URLQueryItem(name: "ip", value: ipString)]
         resourceQueryItems.append(contentsOf: queryItems)
         
         var requestBody = requestData.data(using: .utf8)
-        if useGzipCompression, let gzippedData = requestBody?.gzipped() {
-            requestBody = gzippedData
-            resourceHeaders["Content-Encoding"] = "gzip"
+        if useGzipCompression, let data = requestBody {
+            requestBody = data.gzipped()
         }
-
+        
         let resource = Network.buildResource(path: type.rawValue,
                                              method: .post,
                                              requestBody: requestBody,
